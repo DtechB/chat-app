64d716df851c273d8bd1f033315f082fc033c141
DtechB <d.bayati28@gmail.com>
2022-04-21 16:31:59 +0430

    init project.

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBPGCRn0OXuUt5QlFkFKr_AUg8ikMwp4mI",
+  authDomain: "chat-app-16841.firebaseapp.com",
+  projectId: "chat-app-16841",
+  storageBucket: "chat-app-16841.appspot.com",
+  messagingSenderId: "319337502219",
+  appId: "1:319337502219:web:eac3a48ae008cef0ed590e",
+};
+
+const app = initializeApp(firebaseConfig);
